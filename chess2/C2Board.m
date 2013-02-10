46caea996285dd51b6cad3b402a1a58e537976ae
Berkus <berkus@gmail.com>
2013-02-10 23:30:58 +0200

    Draw the game board, version 1 without outlines.

--- chess2/C2Board.m
+++ chess2/C2Board.m
@@ -25,23 +25,27 @@
 - (void)drawGradientRect:(CGRect)rect forBlackSide:(BOOL)black;
 {
     CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    CGContextClipToRect(context, rect);
     CGGradientRef gradient = black ? [self blackGradient] : [self whiteGradient];
-    CGPoint startPoint = CGPointMake(0.0, 0.0);
+    CGPoint startPoint = rect.origin; //CGPointMake(0.0, 0.0);
     CGPoint endPoint = CGPointMake(CGRectGetMaxX(rect), CGRectGetMaxY(rect));
+    NSLog(@"Drawing gradient rect from %@ to %@", NSStringFromCGPoint(startPoint), NSStringFromCGPoint(endPoint));
     CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
+    CGContextRestoreGState(context);
 }
 
 -(CGGradientRef)blackGradient
 {
     if (nil == _blackGradient)
     {
-        CGFloat colors[6] = {
-            138.0f / 255.0f, 1.0f,
-            162.0f / 255.0f, 1.0f,
-            206.0f / 255.0f, 1.0f};
-        CGFloat locations[3] = { 0.05f, 0.45f, 0.95f };
-        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
-        _blackGradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, 3);
+        CGFloat colors[8] = {
+            130.0f / 255.0f, 121.0f / 255.0f, 127.0f / 255.0f, 1.0f,
+            146.0f / 255.0f, 142.0f / 255.0f, 148.0f / 255.0f, 1.0f,
+        };
+        CGFloat locations[2] = { 0.0f, 1.0f };
+        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+        _blackGradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, 2);
         CGColorSpaceRelease(colorSpace);
     }
     return _blackGradient;
@@ -51,13 +55,13 @@
 {
     if (nil == _whiteGradient)
     {
-        CGFloat colors[6] = {
-            138.0f / 255.0f, 1.0f,
-            162.0f / 255.0f, 1.0f,
-            206.0f / 255.0f, 1.0f};
-        CGFloat locations[3] = { 0.05f, 0.45f, 0.95f };
-        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
-        _whiteGradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, 3);
+        CGFloat colors[8] = {
+            248.0f / 255.0f, 250.0f / 255.0f, 247.0f / 255.0f, 1.0f,
+            253.0f / 255.0f, 255.0f / 255.0f, 252.0f / 255.0f, 1.0f,
+        };
+        CGFloat locations[2] = { 0.0f, 1.0f };
+        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+        _whiteGradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, 2);
         CGColorSpaceRelease(colorSpace);
     }
     return _whiteGradient;
@@ -70,8 +74,17 @@
     // Divide rect into 8x8 subrects
     CGFloat width = (CGRectGetMaxX(rect) - CGRectGetMinX(rect))/8.0;
     CGFloat height = (CGRectGetMaxY(rect) - CGRectGetMinY(rect))/8.0;
+    CGFloat startX = CGRectGetMinX(rect);
+    CGFloat startY = CGRectGetMinY(rect);
+    NSLog(@"startX %f, startY %f, width %f, height %f", startX, startY, width, height);
     
-    [self drawGradientRect:rect forBlackSide:YES];
+    for (int rank = 0; rank < 8; ++rank) {
+        for (int file = 0; file < 8; ++ file) {
+            CGRect r = CGRectMake(startX + width * file, startY + height * rank, width, height);
+            NSLog(@"Drawing a gradient in %@", NSStringFromCGRect(r));
+            [self drawGradientRect:r forBlackSide:!(file%2==rank%2)];
+        }
+    }
 }
 
 @end
