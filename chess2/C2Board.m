f1305000a26869cec6bd65830d7c5cd257437319
Stanislav Kljuhhin <stanislav.kljuhhin@me.com>
2013-02-18 23:38:12 +0100

    Fix compilation
    systemFontWithSize should be systemFontOfSize

--- chess2/C2Board.m
+++ chess2/C2Board.m
@@ -104,7 +104,7 @@
     [boardImage drawInRect:rect];
     
     // Draw board text labels, rank and file.
-    UIFont* font = [UIFont systemFontWithSize:FONT_SIZE];
+    UIFont* font = [UIFont systemFontOfSize:FONT_SIZE];
 
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSaveGState(context);
